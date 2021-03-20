5d6fc19aa8b4324213c4b8bfccfbe8130021a4d3
Ayfer Yıldırım <ayferyildirim3482@gmail.com>
2021-03-20 17:56:22 +0300

    C# Course Project. I created a website backend project with C#. This project connected with Restful API and an example Database. Especially, you can reached this connected codes.

---
+++ Core/Utilities/IoC/ICoreModule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.IoC
+{
+    public class ICoreModule
+    {
+        void Load(IServiceCollection serviceCollection);
+    }
+}
